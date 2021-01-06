21cba1569b7d7bf8323314450502b1e9b1d1b228
FilippoZani <filippo.zani.stud@ispascalcomandini.it>
2021-01-06 16:08:37 +0100

    Creati i thread e i loro comportamenti, sistemati alcuni errori sintattici.

--- Compito_Vacanze_TPS/MainWindow.xaml.cs
+++ Compito_Vacanze_TPS/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace Compito_Vacanze_TPS
 {
@@ -32,13 +33,152 @@ namespace Compito_Vacanze_TPS
         public MainWindow()
         {
             InitializeComponent();
-            ImageSource imgPesce1 = new BitmapImage(uriPesce1);
-            ImageSource imgPesce2 = new BitmapImage(uriPesce2);
-            ImageSource imgPesce3 = new BitmapImage(uriPesce3);
+            ImageSource imggPesce1 = new BitmapImage(uriPesce1);
+            ImageSource imggPesce2 = new BitmapImage(uriPesce2);
+            ImageSource imggPesce3 = new BitmapImage(uriPesce3);
+            r = new Random();
             inizioPesce1 = 49;
             inizioPesce2 = 49;
             inizioPesce3 = 49;
             podio = 0;
+            imgPesce1.Source = imggPesce1;
+            imgPesce2.Source = imggPesce2;
+            imgPesce3.Source = imggPesce3;
+            Thread t1 = new Thread(new ThreadStart(Pesce1));
+            Thread t2 = new Thread(new ThreadStart(Pesce2));
+            Thread t3 = new Thread(new ThreadStart(Pesce3));
+            t1.Start();
+            t2.Start();
+            t3.Start();
+        }
+
+        public void Pesce1()
+        {
+            int sleepingTime = r.Next(30, 81);
+            while (inizioPesce1 < 620)
+            {
+                inizioPesce1 += 1;
+
+                Thread.Sleep(TimeSpan.FromMilliseconds(sleepingTime));
+
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    imgPesce1.Margin = new Thickness(inizioPesce1, 320, 0, 0);
+                }));
+            }
+
+            if (inizioPesce1 >= 680)
+            {
+                Classifica(0);
+            }
+
+        }
+
+        public void Pesce2()
+        {
+            int sleepingTime = r.Next(30, 81);
+            while (inizioPesce2 < 620)
+            {
+                inizioPesce2 += 1;
+
+                Thread.Sleep(TimeSpan.FromMilliseconds(sleepingTime));
+
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    imgPesce2.Margin = new Thickness(inizioPesce2, 320, 0, 0);
+                }));
+            }
+
+            if (inizioPesce2 >= 680)
+            {
+                Classifica(0);
+            }
+
+        }
+
+        public void Pesce3()
+        {
+            int sleepingTime = r.Next(30, 81);
+            while (inizioPesce3 < 620)
+            {
+                inizioPesce3 += 1;
+
+                Thread.Sleep(TimeSpan.FromMilliseconds(sleepingTime));
+
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    imgPesce3.Margin = new Thickness(inizioPesce3, 320, 0, 0);
+                }));
+            }
+
+            if (inizioPesce3 >= 680)
+            {
+                Classifica(0);
+            }
+
+        }
+
+        public void Classifica(int sender)
+        {
+
+            if (sender == 0)
+            {
+                if (podio == 0)
+                {
+                    podio++;
+                    MessageBox.Show("Il boomerang arriva primo");
+                }
+                else if (podio == 1)
+                {
+                    podio++;
+                    MessageBox.Show("Il boomerang arriva secondo");
+                }
+                else if (podio == 2)
+                {
+                    podio++;
+                    MessageBox.Show("Il boomerang arriva terzo");
+                }
+
+            }
+            else if (sender == 1)
+            {
+                if (podio == 0)
+                {
+                    podio++;
+                    MessageBox.Show("Il bladderfish arriva primo");
+                }
+                else if (podio == 1)
+                {
+                    podio++;
+                    MessageBox.Show("Il bladderfish arriva secondo");
+                }
+                else if (podio == 2)
+                {
+                    podio++;
+                    MessageBox.Show("Il bladderfish arriva terzo");
+                }
+
+            }
+            else if (sender == 2)
+            {
+                if (podio == 0)
+                {
+                    podio++;
+                    MessageBox.Show("Il Peeper arriva primo");
+                }
+                else if (podio == 1)
+                {
+                    podio++;
+                    MessageBox.Show("Il Peeper arriva secondo");
+                }
+                else if (podio == 2)
+                {
+                    podio++;
+                    MessageBox.Show("Il Peeper arriva terzo");
+                }
+
+            }
+
         }
     }
 }
